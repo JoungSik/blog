a151dbaaeffd9ea23e0c7c5b4b135a409290cfc4
JoungSik <tjstlr2010@gmail.com>
2021-10-10 01:52:01 +0900

    2021-10-10, 프로그래머스 알고리즘

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# shellcheck disable=SC2164
+cd public/
+git add .
+git commit -m "$1"
+git push origin master
+
+# shellcheck disable=SC2103
+cd ..
+git add .
+git commit -m "$1"
+git push origin master
