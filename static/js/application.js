cbdb2913ba3a796954e3fa2d818b5d88dd1304ca
Joungsik <tjstlr2010@gmail.com>
2025-03-26 00:18:57 +0900

    [Done] Google Analytics 적용

---
+++ static/js/application.js
@@ -0,0 +1,20 @@
+// Google Analytics 초기화
+(function () {
+    // 개발 환경에서는 실행하지 않음 (선택적)
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        console.log('개발 환경에서는 Analytics가 로드되지 않습니다');
+        return;
+    }
+
+    // Google Analytics 초기화
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-DL4SWDGV72');
+
+    // Google Analytics 스크립트 로드
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-DL4SWDGV72';
+    document.head.appendChild(script);
+})();
